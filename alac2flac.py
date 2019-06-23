fbb63059af777bc71f8fc5060a05d739496fde19
Katsuhisa ABE <katsuhisa.abe@gmail.com>
2019-06-23 22:55:47 +0900

    create iterator for alac file

--- alac2flac.py
+++ alac2flac.py
@@ -4,7 +4,23 @@ import os
 import json
 import ffmpeg
 
-class AudioFileDictIterator(object):
+class AudioFileInfo(object):
+    def __init__(self, js):
+        self._js = js
+
+    @property
+    def path(self):
+        return self._js['format']['filename']
+
+    @property
+    def codec_type(self):
+        return self._js['streams'][0]['codec_type']
+    
+    @property
+    def codec_tag_string(self):
+        return self._js['streams'][0]['codec_tag_string']
+
+class AudioFileIterator(object):
     """
     Iterator for dictionary with Audio file
 
@@ -20,12 +36,18 @@ class AudioFileDictIterator(object):
             for fname in file_list:
                 full_path = os.path.join(dir_path, fname)
                 try:
-                    js = ffmpeg.probe(full_path)
-                    if js['streams'][0]['codec_type'] == 'audio':
-                        self._filelists.append({ "path" : full_path,
-                                                 "probe" : js })
+                    info = AudioFileInfo(ffmpeg.probe(full_path))
+                    if self._is_right_file(info):
+                        self._filelists.append(info)
                 except ffmpeg._run.Error:
                     pass
+
+    def _is_right_file(self, audio_file_info):
+        if audio_file_info.codec_type == 'audio':
+            return True
+        else:
+            return False
+        
 
     def __iter__(self):
         return self
@@ -37,25 +59,38 @@ class AudioFileDictIterator(object):
         self._i += 1
         return value
 
+class ALACFileIterator(AudioFileIterator):
+    """
+    Apple Lossless file iterator
+    """
+    def _is_right_file(self, audio_file_info):
+        if audio_file_info.codec_type == 'audio' \
+        and audio_file_info.codec_tag_string == 'alac':
+            return True
+        else:
+            return False
+        
 
 if __name__ == '__main__':
     from os.path import expanduser
     import pickle
 
     iter = None
-    pickle_filename = './audiofiledictiter.pickle'
+    # pickle_filename = './audiofiledictiter.pickle'
+    pickle_filename = './alac_file_iterator.pickle'
     try:
         with open(pickle_filename, 'rb') as rf:
             iter = pickle.load(rf)
     except FileNotFoundError:
         home = expanduser("~")
-        iter = AudioFileDictIterator(home + "/Music/")    
+        # iter = AudioFileIterator(home + "/Music/")
+        iter = ALACFileIterator(home + "/Music/")    
         with open(pickle_filename, 'wb') as wf:
             pickle.dump(iter, wf)
         
-    for dict in iter:
-        print(dict['probe']['streams'][0])
-        print(dict['path'])
+    for fileinfo in iter:
+        # print(dict)
+        print(fileinfo.path)
 
 
 
